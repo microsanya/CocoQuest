45a60420ef1925729be8e14b54c11eaee215e6f1
microsanya <159100376+microsanya@users.noreply.github.com>
2026-06-28 01:11:13 +0700

    Initial commit

---
+++ CocoQuest/Forms/MainForm.cs
@@ -0,0 +1,10 @@
+namespace CocoQuest
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CocoQuest/Utils/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CocoQuest.Utils
+{
+    public static class Constants
+    {
+        public const int WorkoutXP = 150;
+        public const int WaterXP = 20;
+        public const int ProteinXP = 40;
+    }
+}
